52c8b3ce6ee64683503c6dd4da4aa135d09e324b
VigorousyT <harshyadav082000@gmail.com>
2023-01-15 11:01:46 +0530

    portfolio.react.app.{upto About}

--- portfolio-react-app/src/components/Project/Project.jsx
+++ portfolio-react-app/src/components/Project/Project.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Link from "react-router-dom";
 
 function Project() {
   return (
     <div>
-      <h1>Proj.</h1>
+      <h1>405 - Under Development...</h1>
+      <Link to="/">Go to Home</Link>
     </div>
   );
 }
